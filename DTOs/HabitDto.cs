2c4b0fc31ed6650c688f5e5db282dcd75aa6ec05
abramepithey <abramepithey@gmail.com>
2025-05-01 21:01:32 -0600

    Add DTO classes for Habit, HabitEntry, Login, and Register

---
+++ DTOs/HabitDto.cs
@@ -0,0 +1,6 @@
+namespace MinimalApi.DTOs;
+
+public class HabitDto
+{
+    public string Name { get; set; } = string.Empty;
+} 
---
+++ DTOs/HabitEntryDto.cs
@@ -0,0 +1,7 @@
+namespace MinimalApi.DTOs;
+
+public class HabitEntryDto
+{
+    public DateTime Date { get; set; }
+    public bool Completed { get; set; }
+} 
